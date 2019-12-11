2f6e94ab5d256ebf5168f21999515c850937a572
Kazuhisa Ishizaka <ishizaka@nec.com>
2019-12-11 17:07:46 +0900

    Add performance check tools and database

---
+++ doc/perf.md
@@ -0,0 +1,113 @@
+# Performance test
+
+`perf.py` is a script to run benchmark and check a result with latest result in
+database.
+
+## Design
+
+A benchmark program is `bench` that is build in `<build direcotry>/test/bench`.
+
+A result of benchmark is a simple text file like below. '# ts ..' line is
+timestamp and mandatory.
+
+```
+Add                                                                                 1.151 ms
+Sub                                                                                 1.063 ms
+(snip)
+Conv2D-32x256x34x34-512x256x4x4-32x512x31x31-1x1-1x1-0x0-1-1                      114.551 ms
+Conv2DBackpropInput-32x1024x16x16-1024x256x4x4-32x256x32x32-2x2-1x1-1x1-1-1       121.133 ms
+# ts: 2019-12-11T16:39:36+0900
+# hostname: aurora-ds01
+# user: ishizaka
+```
+
+A database is group of result files typically stored in `prefdb` directory.
+A database should be shared among team members through git.
+
+## Typical usage
+
+Run benchmark and compare result with latest result in a database. Benchmark
+result is stored in `hoge` file.
+
+```
+% python perf.py -e build-2.4.1/test/bench test -o hoge
+```
+
+If ok, store a result in a database. filename in a database is automatically
+created from timestamp of a result.
+
+```
+% python bench.py store hoge
+```
+
+Then add new result into git repository.
+
+## Other usages
+
+```
+   # Run benchmark, check and write result to <filename>
+   % python perf.py [-e executable] test [-o filename]
+   
+   # Store <filename> to database
+   % python perf.py store <filename>
+
+   # Show results in a database
+   % python perf.py show [-l num]
+
+   # Compare latest two results in a database
+   % python perf.py check [-v]
+
+   # Compare a result in <filename> with latest result in a database
+   % python perf.py check [-v] -a <filename>
+
+   # Compare two results
+   % python perf.py check [-v] -a <filename> -b <filename>
+
+   # Run benchmark and output result
+   % python perf.py run
+```
+
+## Options
+
+```
+   -e <filename>    benchmark executable [build/test/bench]
+   -d <directory>   directory of database [perfdb]
+   -t <threshold>   threshold in percent [5]
+   -o <filename>    filename to save a benchmark result
+   -v               increses verbose level
+```
+
+## Example
+
+```
+% python perf.py -e build-2.4.1/test/bench test -o hoge
+a: None (2019-12-11 17:00:37+09:00)
+b: perfdb/2019-12-11T07:39:36+0000 (2019-12-11 16:39:36+09:00)
+All Results:
+benchmark                                                                        a        b        diff     diff%
+Add                                                                                 1.061    1.151   -0.090   -7.819 %
+Sub                                                                                 1.046    1.063   -0.017   -1.599 %
+Mul                                                                                 0.265    0.267   -0.002   -0.749 %
+Div                                                                                 1.497    1.465    0.032    2.184 %
+Mean                                                                                0.767    0.773   -0.006   -0.776 %
+Sum                                                                                 0.751    0.746    0.005    0.670 %
+Neg                                                                                 0.178    0.178    0.000    0.000 %
+Rsqrt                                                                               0.178    0.177    0.001    0.565 %
+Sqrt                                                                                0.531    0.523    0.008    1.530 %
+Square                                                                              0.179    0.180   -0.001   -0.556 %
+BiasAdd(NHWC)                                                                       0.522    0.531   -0.009   -1.695 %
+BiasAdd(NCHW)                                                                       0.158    0.158    0.000    0.000 %
+BiasAddGrad(NHWC)                                                                   3.791    3.731    0.060    1.608 %
+BiasAddGrad(NCHW)                                                                   0.330    0.331   -0.001   -0.302 %
+Tile                                                                                0.066    0.066    0.000    0.000 %
+Transpose(0231)                                                                     0.230    0.228    0.002    0.877 %
+Transpose(0312)                                                                     0.435    0.417    0.018    4.317 %
+ApplyAdam                                                                           0.629    0.626    0.003    0.479 %
+Conv2D-32x256x34x34-512x256x4x4-32x512x31x31-1x1-1x1-0x0-1-1                      114.541  114.551   -0.010   -0.009 %
+Conv2DBackpropInput-32x1024x16x16-1024x256x4x4-32x256x32x32-2x2-1x1-1x1-1-1       121.149  121.133    0.016    0.013 %
+0 errors found
+write result to hoge
+
+% python perf.py -e build-2.4.1/test/bench store hoge
+Copy hoge to perfdb/2019-12-11T08:00:37+0000
+```
---
+++ perf.py
@@ -0,0 +1,244 @@
+import glob
+import os
+import argparse
+import subprocess
+import getpass
+import re
+import tzlocal
+from datetime import datetime
+from datetime import timezone
+
+FMT = '%Y-%m-%dT%H:%M:%S%z'
+
+class Bench:
+  def __init__(self, header, table):
+    self.header = header
+    self.table = table
+
+  def to_s(self):
+    return "{} ({})".format(self.filename(), self.ts())
+
+  def set(self, key, value):
+    self.header[key] = value
+
+  def get(self, key):
+    return self.header[key] if key in self.header else None
+
+  def ts(self): return self.get("ts")
+  def filename(self): return self.get("filename")
+
+  def text(self):
+    s = ""
+    for k, v in self.table.items():
+      s += "{:<80} {:8.3f} ms\n".format(k, v)
+    for k, v in self.header.items():
+      if isinstance(v, datetime):
+        s += "# {}: {}\n".format(k, v.strftime(FMT))
+      else:
+        s += "# {}: {}\n".format(k, v)
+    return s
+
+  def print(self):
+    print(self.text(), end="")
+
+  @classmethod
+  def parse(cls, text):
+    header = {}
+    t = {}
+    for line in text.splitlines():
+      if line[0] == "#":
+        k, v = line[1:].strip().split(":", 1)
+        header[k.strip()] = v.strip()
+      else:
+        name, time, *others = line.strip().split()
+        t[name] = float(time)
+
+    if "ts" in header:
+      #header["ts"] = datetime.strptime(header["ts"], '%Y-%m-%dT%H:%M:%S.%f%z')
+      header["ts"] = datetime.strptime(header["ts"], FMT)
+    return Bench(header, t)
+
+  @classmethod
+  def parse_file(cls, filename):
+    with open(filename) as f:
+      bench = Bench.parse(f.read())
+      bench.set("filename", filename)
+      return bench
+
+class Diff:
+  def __init__(self, a, b, k):
+    self.key = k
+    self.va = a.table[k]
+    if b:
+      self.vb = b.table[k]
+      self.diff = self.va - self.vb
+      self.ratio = self.diff / self.vb * 100
+
+  def to_s(self):
+    return "{:<80} {:8.3f} {:8.3f} {:8.3f} {:8.3f} %".format(self.key, self.va, self.vb, self.diff, self.ratio)
+
+class CompareResult:
+  def __init__(self, a, b, diffs, threshold):
+    self.a = a
+    self.b = b
+    self.diffs = diffs
+    self.threshold = threshold
+
+  def is_ok(self, threshold):
+    return len(self.errors(threshold)) == 0
+
+  def errors(self, threshold):
+    e = []
+    for d in self.diffs:
+      if d.ratio > threshold:
+        e.append(d)
+    return e
+
+  def print(self, print_all=False):
+    header ="{:<80} {:<8} {:<8} {:<8} {:<8}".format("benchmark", 
+                                                    "a", "b", "diff", "diff%")
+    print("a: {}".format(self.a.to_s()))
+    print("b: {}".format(self.b.to_s()))
+
+    if print_all:
+      print("All Results:")
+      print(header)
+      for d in self.diffs:
+        print(d.to_s())
+
+    errors = self.errors(self.threshold)
+    print("{} errors found".format(len(errors)))
+    if len(errors) > 0:
+      print(header)
+      for d in errors:
+        print(d.to_s())
+
+def load(args):
+  files = args.files
+  if args.dir:
+    files.extend(glob.glob(args.dir + "/*"))
+
+  ary = []
+  for f in files:
+    ary.append(Bench.parse_file(f))
+
+  return sorted(ary, key=lambda x : x.ts()) 
+
+def compare(args, a=None, b=None):
+  if not a or not b:
+    ary = load(args)
+    if a:
+      b = ary[-1]
+    else:
+      a = ary[-1]
+      b = ary[-2]
+
+  if not a or not b:
+    raise("no enough result")
+  else:
+    return CompareResult(a, b, [Diff(a, b, k) for k in a.table.keys()], 
+                         args.threshold)
+
+def run(args):
+  exe = args.exe
+  try:
+    ret = subprocess.run(exe, check=True, stdout=subprocess.PIPE, 
+                         universal_newlines=True)
+    text = ret.stdout
+    text += "# ts: {}\n".format(datetime.now(tz=tzlocal.get_localzone()).strftime(FMT))
+    text += "# hostname: {}\n".format(os.uname().nodename)
+    text += "# user: {}\n".format(getpass.getuser())
+    return Bench.parse(text)
+  except subprocess.CalledProcessError as e:
+    print(e)
+
+def save(args, bench, filename):
+  with open(filename, "w") as f:
+    f.write(bench.text())
+
+# Command
+
+def test(args):
+  bench = run(args)
+  result = compare(args, bench)
+  result.print(True)
+  if args.output:
+    print("write result to {}".format(args.output))
+    save(args, bench, args.output)
+
+def store(args):
+  if len(args.files) == 0:
+    return # do nothing
+
+  frm = args.files[0]
+
+  bench = Bench.parse_file(args.files[0])
+  to = "{}/{}".format(args.dir, 
+                      bench.ts().astimezone(timezone.utc).strftime(FMT))
+  print("Copy {} to {}".format(frm, to))
+  save(args, bench, to)
+
+def check(args):
+  a = None
+  b = None
+  if args.a:
+    a = Bench.parse_file(args.a)
+  if args.b:
+    b = Bench.parse_file(args.b)
+  compare(args, a, b).print(args.verbose > 0)
+
+def show(args):
+  ary = load(args)
+  #print("show: len={}".format(len(ary)))
+  if len(ary) < 0:
+    return
+  tmp = list(reversed(ary))[0:args.l]
+  a = tmp[-1]
+
+  for i, b in enumerate(tmp):
+    print("{} {}".format(i, b.to_s()))
+
+  print("{:80}".format(""), end="")
+  for i in range(len(tmp)):
+    print(" {:<8}".format(i), end="")
+  print()
+
+  for k in a.table.keys():
+    print("{:<80}".format(k), end="")
+    for b in tmp:
+      print(" {:8.3f}".format(b.table[k]), end="")
+    print()
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-t", "--threshold", type=float, default=5.0)
+  parser.add_argument("-v", "--verbose", action="count", default=0)
+  parser.add_argument("-d", "--dir", type=str, default="perfdb")
+  parser.add_argument("-e", "--exe", type=str, default="build/test/bench")
+  parser.add_argument("-l", type=int, default=5)
+  parser.add_argument("-a", type=str)
+  parser.add_argument("-b", type=str)
+  parser.add_argument("-o", "--output", type=str)
+  parser.add_argument("command", type=str, nargs="?")
+  parser.add_argument("files", nargs="*")
+  args = parser.parse_args()
+
+  #print(args.command)
+  if args.command == "check":
+    check(args)
+  elif args.command == "parse": # check if file can be parsed
+    for f in args.files:
+      Bench.parse_file(f)
+  elif args.command == "run":
+    b = run(args)
+    b.print()
+  elif args.command == "test": # run and check
+    test(args)
+  elif args.command == "store":
+    store(args)
+  elif args.command == "show":
+    show(args)
+
+if __name__ == "__main__":
+  main()
