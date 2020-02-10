d5557271e7106c8e86ab7675c5ce02cc7196cdc0
Yoshiyuki Ohno <ohno.yoshiyuki@nec.com>
2020-02-10 17:54:35 +0900

    update biasAdd

--- vml/src/intrinsic/BiasAdd.c
+++ vml/src/intrinsic/BiasAdd.c
@@ -11,49 +11,164 @@
 #define NCHW_IDX(n,c,h,w,cl,hl,wl) ((((n)*(cl)+(c))*(hl)+(h))*(wl)+(w))
 
 
-
-int BiasAdd_NHWC_f32(uint64_t out, uint64_t in, uint64_t bias, int batch, int width, int height, int channel)
+static inline void biasadd_nhwc_vectc_cu1mvl(
+    float *pout,
+    const float *pin,
+    const float *pbias,
+    const size_t bhw,
+    const size_t channel,
+    const size_t c,
+    const size_t remvl )
 {
-	float* pout = (float*)(out);
-	const float* pin = (const float*)(in);
-	const float* pbias = (const float*)(bias);
+  __vr vrb0 = _vel_vldu_vssl(4, pbias+c+0*256, remvl);
+  for (size_t b = 0; b < bhw; ++b) {
+    __vr vri0  = _vel_vldu_vssl(4, pin+b*channel+c+0*256, remvl);
 
-	if((channel<64)&&(width*height>channel)){
-		for (int xy = 0; xy < width*height; xy+=VLEN) {
-			const int64_t vlen = width*height-xy < VLEN ? width*height-xy : VLEN;
-			 ;
-			for (int b = 0; b < batch; ++b) {
-				int i = b * height * width * channel
-					+ xy * channel;
-				for (int c = 0; c < channel; ++c) {
-					__vr vr_pin = _vel_vldu_vssl(4*channel,pin+i+c, vlen);
-					__vr vr_sum = _vel_vfadds_vsvl(pbias[c], vr_pin, vlen);
-					_vel_vstu_vssl(vr_sum,4*channel,pout+i+c, vlen);
-				}
-			}
-		}
-	}else{
-		for (int c = 0; c < channel; c+=VLEN) {
-			const int64_t vlen = channel-c < VLEN ? channel-c : VLEN;
-			 ;
-			__vr vr_pbias = _vel_vldu_vssl(4,pbias+c, vlen);
-			for (int b = 0; b < batch; ++b) {
-				for (int xy = 0; xy < width*height; xy++) {
-					int i = b * height * width * channel
-						+ xy * channel;
-					__vr vr_pin = _vel_vldu_vssl(4,pin+i+c, vlen);
-					__vr vr_sum = _vel_vfadds_vvvl(vr_pbias, vr_pin, vlen);
-					_vel_vstu_vssl(vr_sum,4,pout+i+c, vlen);
-				}
-			}
-		}
+    __vr vro0 = _vel_vfadds_vvvl(vrb0, vri0, remvl);
 
+    _vel_vstu_vssl(vro0, 4, pout+b*channel+c+0*256, remvl);
+  }
+}
+
+static inline void biasadd_nhwc_vectc_cu2mvl(
+    float *pout,
+    const float *pin,
+    const float *pbias,
+    const size_t bhw,
+    const size_t channel,
+    const size_t c,
+    const size_t remvl )
+{
+  __vr vrb0 = _vel_vldu_vssl(4, pbias+c+0*256, 256);
+  __vr vrb1 = _vel_vldu_vssl(4, pbias+c+1*256, remvl);
+  for (size_t b = 0; b < bhw; ++b) {
+    __vr vri0  = _vel_vldu_vssl(4, pin+b*channel+c+0*256, 256);
+    __vr vri1  = _vel_vldu_vssl(4, pin+b*channel+c+1*256, remvl);
+
+    __vr vro0 = _vel_vfadds_vvvl(vrb0, vri0, 256);
+    __vr vro1 = _vel_vfadds_vvvl(vrb1, vri1, remvl);
+
+    _vel_vstu_vssl(vro0, 4, pout+b*channel+c+0*256, 256);
+    _vel_vstu_vssl(vro1, 4, pout+b*channel+c+1*256, remvl);
+  }
+}
+
+static inline void biasadd_nhwc_vectc_cu3mvl(
+    float *pout,
+    const float *pin,
+    const float *pbias,
+    const size_t bhw,
+    const size_t channel,
+    const size_t c,
+    const size_t remvl )
+{
+  __vr vrb0 = _vel_vldu_vssl(4, pbias+c+0*256, 256);
+  __vr vrb1 = _vel_vldu_vssl(4, pbias+c+1*256, 256);
+  __vr vrb2 = _vel_vldu_vssl(4, pbias+c+2*256, remvl);
+  for (size_t b = 0; b < bhw; ++b) {
+    __vr vri0  = _vel_vldu_vssl(4, pin+b*channel+c+0*256, 256);
+    __vr vri1  = _vel_vldu_vssl(4, pin+b*channel+c+1*256, 256);
+    __vr vri2  = _vel_vldu_vssl(4, pin+b*channel+c+2*256, remvl);
+
+    __vr vro0 = _vel_vfadds_vvvl(vrb0, vri0, 256);
+    __vr vro1 = _vel_vfadds_vvvl(vrb1, vri1, 256);
+    __vr vro2 = _vel_vfadds_vvvl(vrb2, vri2, remvl);
+
+    _vel_vstu_vssl(vro0, 4, pout+b*channel+c+0*256, 256);
+    _vel_vstu_vssl(vro1, 4, pout+b*channel+c+1*256, 256);
+    _vel_vstu_vssl(vro2, 4, pout+b*channel+c+2*256, remvl);
+  }
+}
+
+static inline void biasadd_nhwc_vectc_cu4mvl(
+    float *pout,
+    const float *pin,
+    const float *pbias,
+    const size_t bhw,
+    const size_t channel,
+    const size_t c,
+    const size_t remvl )
+{
+  __vr vrb0 = _vel_vldu_vssl(4, pbias+c+0*256, 256);
+  __vr vrb1 = _vel_vldu_vssl(4, pbias+c+1*256, 256);
+  __vr vrb2 = _vel_vldu_vssl(4, pbias+c+2*256, 256);
+  __vr vrb3 = _vel_vldu_vssl(4, pbias+c+3*256, remvl);
+  for (size_t b = 0; b < bhw; ++b) {
+    __vr vri0  = _vel_vldu_vssl(4, pin+b*channel+c+0*256, 256);
+    __vr vri1  = _vel_vldu_vssl(4, pin+b*channel+c+1*256, 256);
+    __vr vri2  = _vel_vldu_vssl(4, pin+b*channel+c+2*256, 256);
+    __vr vri3  = _vel_vldu_vssl(4, pin+b*channel+c+3*256, remvl);
+
+    __vr vro0 = _vel_vfadds_vvvl(vrb0, vri0, 256);
+    __vr vro1 = _vel_vfadds_vvvl(vrb1, vri1, 256);
+    __vr vro2 = _vel_vfadds_vvvl(vrb2, vri2, 256);
+    __vr vro3 = _vel_vfadds_vvvl(vrb3, vri3, remvl);
+
+    _vel_vstu_vssl(vro0, 4, pout+b*channel+c+0*256, 256);
+    _vel_vstu_vssl(vro1, 4, pout+b*channel+c+1*256, 256);
+    _vel_vstu_vssl(vro2, 4, pout+b*channel+c+2*256, 256);
+    _vel_vstu_vssl(vro3, 4, pout+b*channel+c+3*256, remvl);
+  }
+}
+
+int BiasAdd_NHWC_f32(uint64_t out, uint64_t in, uint64_t bias, int batch,
+    int width, int height, int channel) {
+  float* pout = (float*) (out);
+  const float* pin = (const float*) (in);
+  const float* pbias = (const float*) (bias);
+
+  if ((channel < 64) && (width * height > channel)) {
+    for (int xy = 0; xy < width * height; xy += VLEN) {
+      const int64_t vlen =
+	  width * height - xy < VLEN ? width * height - xy : VLEN;
+
+      for (int b = 0; b < batch; ++b) {
+	int i = b * height * width * channel + xy * channel;
+	for (int c = 0; c < channel; ++c) {
+	  __vr vr_pin = _vel_vldu_vssl(4 * channel, pin + i + c, vlen);
+	  __vr vr_sum = _vel_vfadds_vsvl(pbias[c], vr_pin, vlen);
+	  _vel_vstu_vssl(vr_sum, 4 * channel, pout + i + c, vlen);
 	}
+      }
+    }
+  } else {
+    size_t bhw = ((size_t) batch) * width * height ;
+    size_t cremain = channel % (4*256) ;
+
+    size_t c = 0 ;
+    if ( cremain > 0 ) {
+      if( cremain <= 1*256 ) {
+	size_t remvl = cremain % 256 == 0 ? 256 : cremain % 256 ;
+	biasadd_nhwc_vectc_cu1mvl(pout, pin, pbias, bhw, channel, c, remvl ) ;
+	c += cremain ;
+      }
+      else if( cremain <= 2*256 ) {
+	size_t remvl = cremain % 256 == 0 ? 256 : cremain % 256 ;
+	biasadd_nhwc_vectc_cu2mvl(pout, pin, pbias, bhw, channel, c, remvl ) ;
+	c += cremain ;
+      }
+      else if( cremain <= 3*256 ) {
+	size_t remvl = cremain % 256 == 0 ? 256 : cremain % 256 ;
+	biasadd_nhwc_vectc_cu3mvl(pout, pin, pbias, bhw, channel, c, remvl ) ;
+	c += cremain ;
+      }
+      else { // cremain  < 4*256
+	size_t remvl = cremain % 256 == 0 ? 256 : cremain % 256 ;
+	biasadd_nhwc_vectc_cu4mvl(pout, pin, pbias, bhw, channel, c, remvl ) ;
+	c += cremain ;
+      }
+    }
+
+    while( c < channel ) {
+      biasadd_nhwc_vectc_cu4mvl(pout, pin, pbias, bhw, channel, c, 256 ) ;
+      c += 4*256 ;
+    }
+  }
 
 #if 0
-	fprintf(stderr, "%s done\n", __PRETTY_FUNCTION__);
+  fprintf(stderr, "%s done\n", __PRETTY_FUNCTION__);
 #endif
-	return 0;
+  return 0;
 }
 
 
